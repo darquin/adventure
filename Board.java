99f4c82c7b902fdc7e55f1a2d14cdd2cd31952f8
Antti Holvikari <anttih@gmail.com>
2010-03-09 16:39:08 +0200

    Luodaan golemeita listaan automaattisesti

--- Board.java
+++ Board.java
@@ -7,6 +7,10 @@ import java.awt.event.KeyListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.ListIterator;
+
 import java.awt.Point;
 
 import javax.swing.JPanel;
@@ -19,7 +23,8 @@ public class Board extends JPanel implements KeyListener {
     
     private static final long serialVersionUID = 1L;
     private Creature hero;
-    private Creature golem;
+    //private Creature golem;
+    private ArrayList creatures;
     
     private Map map;
     
@@ -36,12 +41,21 @@ public class Board extends JPanel implements KeyListener {
         this.area = new Point(0, 0);
         
         this.map = new Map();
-        this.hero = new Creature(0, 0, "creature");
+        this.hero = new Creature(4, 4, "creature");
         this.ai = new AI();
         
-        this.golem = new Creature(3, 3, "golem");
-        this.golem.setMap(this.map);
-        this.golem.setTarget(this.hero);
+        // this.golem = new Creature(3, 3, "golem");
+        // this.golem.setMap(this.map);
+        // this.golem.setTarget(this.hero);
+        
+        this.creatures = new ArrayList();
+        
+        for (int i = 0; i < 5; i++) {
+            Creature golem = new Creature(8, 2+i, "golem");
+            golem.setMap(this.map);
+            golem.setTarget(this.hero);
+            this.creatures.add(golem);
+        }
         
         this.hero.setMap(map);
         Dimension dimension = new Dimension(Map.WIDTH, Map.HEIGHT); 
@@ -60,21 +74,29 @@ public class Board extends JPanel implements KeyListener {
         int vy = (int) this.area.getY();
         
         // pitää olla koko mapin sisällä jotta edes testataan kartan siirtymistä
-        boolean insidemap = (x > (buffer - 1) && x < (Map.TOTALCOLS - buffer)) && (y > (buffer - 1) && y < (Map.TOTALROWS - buffer));
-            
-        boolean insideview = (x > (vx + buffer - 1) && x < (vx + Map.COLS - buffer)) && (y > (vy + buffer - 1) && y < (vy + Map.ROWS - buffer));
+        boolean insidemap = (x > (buffer - 1) && x < (Map.TOTALCOLS - buffer))
+                          || (y > (buffer - 1) && y < (Map.TOTALROWS - buffer));
+
+        
+        boolean insideview = (x > (vx + buffer - 1) && x < (vx + Map.COLS - buffer))
+                           && (y > (vy + buffer - 1) && y < (vy + Map.ROWS - buffer));
         
         if (! insideview && insidemap) {
             int new_view_x = x - 10;
             int new_view_y = y - 7;
-            
+
             this.area.setLocation(new_view_x, new_view_y);
         }
         
         this.map.paint(g, this.area, this);
         
         this.hero.paint(this.area, g, this);
-        this.golem.paint(this.area, g, this);
+        
+        ListIterator<Creature> iter = this.creatures.listIterator();
+        while (iter.hasNext()) {
+            iter.next().paint(this.area, g, this);
+        }
+        //this.golem.paint(this.area, g, this);
     }
     
     public void keyTyped(KeyEvent e)
@@ -93,7 +115,12 @@ public class Board extends JPanel implements KeyListener {
         
         this.hero.move(key);
         
-        this.ai.act(this.golem);
+        //this.ai.act(this.golem);
+        
+        ListIterator<Creature> iter = this.creatures.listIterator();
+        while (iter.hasNext()) {
+            this.ai.act(iter.next());
+        }
         
         repaint();
     }
